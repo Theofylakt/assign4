02d3a6067d25aef471a7dbb8c04fd5616bef5202
chrisgit98 <90904750+chrisgit98@users.noreply.github.com>
2021-10-28 15:49:53 +0200

    Add files via upload

---
+++ RAWDATA_2021_Livecode4/EfEx/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+namespace Assignment4.Tests
+{
+    public class DataServiceTests
+    {
+        public void Order_Object_HasIdDatesAndOrderDetails()
+        {
+            var order = new Order();
+            Assert.Equal(0, order.Id);
+            Assert.Equal(new DateTime(), order.Date);
+            Assert.Equal(new DateTime(), order.Required);
+            Assert.Null(order.OrderDetails);
+            Assert.Null(order.ShipName);
+            Assert.Null(order.ShipCity);
+        }
+    }
+}
---
+++ RAWDATA_2021_Livecode4/EfEx/DataService.cs
@@ -0,0 +1,54 @@
+﻿using EfEx.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfEx
+{
+
+    public interface IDataService
+    {
+        IList<Category> GetCategories();
+        bool CreateCategory(Category category);
+
+        IList<Products> GetProducts();
+
+    }
+    public class DataService : IDataService
+    {
+        public bool CreateCategory(Category category)
+        {
+            var ctx = new NorthwindContext();
+            category.Id = ctx.Categories.Max(x => x.Id) + 1;
+            ctx.Add(category);
+            return ctx.SaveChanges() > 0;
+        }
+
+        public IList<Category> GetCategories()
+        {
+            var ctx = new NorthwindContext();
+            return ctx.Categories.ToList();
+        }
+
+        public IList<Products> GetProducts()
+        {
+            var ctx = new NorthwindContext();
+            return ctx.Products.ToList();
+        }
+
+        public IList<Order> GetOrder()
+        {
+            var ctx = new NorthwindContext();
+            return ctx.Order.ToList();
+        }
+
+        public IList<OrderDetails> GetOrderDetails()
+        {
+            var ctx = new NorthwindContext();
+            return ctx.OrderDetails.ToList();
+        }
+
+    }
+}
